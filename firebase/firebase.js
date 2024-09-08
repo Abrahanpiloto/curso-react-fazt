31e08dddc0101f23ef0e0ab8b53a93a004fead29
Abrahan <abrahanpiloto@gmail.com>
2024-09-08 10:58:26 -0500

    implementacion de firebase

---
+++ firebase/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfWOa0-m_8UkdPkWQ0-x6zsJ-wxDH9wdU",
+  authDomain: "tasks-list-c394a.firebaseapp.com",
+  projectId: "tasks-list-c394a",
+  storageBucket: "tasks-list-c394a.appspot.com",
+  messagingSenderId: "854207177106",
+  appId: "1:854207177106:web:dae672c0ce210251d2214b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializa Firestore
+const db = getFirestore(app);
+
+export { db };
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,16 @@ import TaskList from "../components/TaskList";
 import Modal from "../components/Modal";
 import { useEffect, useState } from "react";
 import { tasks as data } from "../tasks";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+// import { db } from "../firebase"; // Asegúrate de que el archivo de configuración esté bien ubicado
+import { db } from "../../firebase/firebase";
 import "../styles/App.css";
 
 function App() {
@@ -11,29 +21,31 @@ function App() {
   const [taskToDelete, setTaskToDelete] = useState(null);
   const [showModal, setShowModal] = useState(false);
 
+  // Obtener tareas desde Firestore al cargar la página:
   useEffect(() => {
-    const storedTasks = localStorage.getItem("tasks");
-    if (storedTasks) {
-      setTasks(JSON.parse(storedTasks));
-    } else {
-      setTasks(data);
-    }
+    const fetchTasks = async () => {
+      const querySnapshot = await getDocs(collection(db, "tasks"));
+      const tasksArray = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setTasks(tasksArray);
+    };
+    fetchTasks();
   }, []);
 
-  // funcion que crea una nueva tarea:
-  const createTask = (title, description) => {
-    const updatedTasks = [
-      ...tasks,
-      {
-        id: tasks.length,
-        title,
-        description,
-        createdAt: new Date().toLocaleString(),
-      },
-    ];
-    setTasks(updatedTasks);
-    //Guarda en el localstorage:
-    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+  // Función para crear una nueva tarea en Firebase:
+  const createTask = async (title, description) => {
+    const newTask = {
+      title,
+      description,
+      createdAt: new Date().toLocaleString(),
+    };
+    //Agrega la tarea a Firestore:
+    const docRef = await addDoc(collection(db, "tasks"), newTask);
+
+    //Actualiza el estado con la nueva tarea:
+    setTasks([...tasks, { id: docRef.id, ...newTask }]);
   };
 
   // Función que solicita confirmación para eliminar una tarea:
@@ -41,11 +53,13 @@ function App() {
     setTaskToDelete(taskId);
     setShowModal(true);
   };
-  // funcion que elimina una tarea:
-  const deleteTask = (taskId) => {
-    const updatedTasks = tasks.filter((task) => task.id !== taskId);
-    setTasks(updatedTasks);
-    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+
+  //Función para eliminar una tarea desde Firebase:
+  const deleteTask = async (taskId) => {
+    await deleteDoc(doc(db, "tasks", taskId));
+    //Actualiza el estado localmente:
+    const updatedTask = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTask);
     setShowModal(false);
   };
 
@@ -57,14 +71,23 @@ function App() {
     setTaskToEdit(task); //Configura la tarea para editar
   };
 
-  const updateTask = (updateTask) => {
+  //Funcion que actualiza una tarea en Firebase
+  const updateTask = async (updatedTask) => {
+    // Actualiza la tarea en Firestore
+    const taskRef = doc(db, "tasks", updatedTask.id);
+    await updateDoc(taskRef, {
+      title: updatedTask.title,
+      description: updatedTask.description,
+    });
+
+    // Actualiza el estado localmente
     const updatedTasks = tasks.map((task) =>
-      task.id === updateTask.id ? updateTask : task
+      task.id === updatedTask.id ? updatedTask : task
     );
     setTasks(updatedTasks);
-    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
-    setTaskToEdit(null); //reinicia el formulario despues de la edicion
+    setTaskToEdit(null); // Reinicia el formulario después de la edición
   };
+
   return (
     <div>
       <TaskForm
