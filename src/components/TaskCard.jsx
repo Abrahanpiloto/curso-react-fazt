cdc9a27fe4fcdbf15c19005b79b958b641e5cacb
Abrahan Piloto <abrahanpiloto@gmail.com>
2024-09-10 22:47:04 -0500

    css-buttons

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -8,8 +8,17 @@ function TaskCard({ task, deleteTask, editTask }) {
       <h2 className="task-card-header">{capitalizeFirstLetter(task.title)}</h2>
       <p className="task-card-body">{task.description}</p>
       <p className="time">{task.createdAt}</p>
-      <button onClick={() => deleteTask(task.id)}>Delete</button>
-      <button onClick={() => editTask(task)}>Edit</button>
+      <div className="button-container">
+        <button
+          className="button-card-delete"
+          onClick={() => deleteTask(task.id)}
+        >
+          Delete
+        </button>
+        <button className="button-card-edit" onClick={() => editTask(task)}>
+          Edit
+        </button>
+      </div>
     </div>
   );
 }
