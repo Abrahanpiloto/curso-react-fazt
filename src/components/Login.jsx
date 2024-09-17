52beba653496c8372e6738f76eb209174615a269
Abrahan <abrahanpiloto@gmail.com>
2024-09-17 18:18:29 -0500

    cambio de login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../../firebase/firebase";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import beach from "../images/beach.jpg";
 import flag from "../images/flag.jpg";
 import merida from "../images/merida.jpg";
@@ -11,19 +16,45 @@ import "../styles/Login.css";
 const Login = () => {
   const [error, setError] = useState("");
   const navigate = useNavigate(); // Hook para redireccionar
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isRegistering, setIsRegistering] = useState(false);
+  const [showPassword, setShowPassword] = useState(false); // Estado para mostrar/ocultar la contraseña
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false); // Estado para la confirmación
 
-  const handleGoogleLogin = async () => {
-    const provider = new GoogleAuthProvider();
+  const handleLogin = async () => {
     try {
-      await signInWithPopup(auth, provider);
+      await signInWithEmailAndPassword(auth, email, password);
       navigate("/tasks"); // Redirige a la página de tareas después del inicio de sesión
     } catch (err) {
       setError(err.message);
     }
   };
 
+  const handleRegister = async () => {
+    if (password !== confirmPassword) {
+      setError("Las contraseñas no coinciden");
+      return;
+    }
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      navigate("/tasks");
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword); // Alterna entre mostrar y ocultar contraseña
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword); // Alterna entre mostrar y ocultar confirmación
+  };
+
   return (
     <div className="row container p-4 h-100">
+      {/* esta seccion es para el Carrusel de imagenes */}
       <div className="col-md-8 h-100 d-flex align-items-center justify-content-center">
         <div id="carouselExampleIndicators" className="carousel slide h-100">
           <div className="carousel-indicators">
@@ -96,32 +127,63 @@ const Login = () => {
       {/* esta seccion sera para el login */}
       <div className="col-md-4 h-100 d-flex align-items-center justify-content-center">
         <div className="login-box">
-          <h2>Login</h2>
-          <button className="button" onClick={handleGoogleLogin}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="xMidYMid"
-              viewBox="0 0 256 262"
+          <h2>{isRegistering ? "Registrar nuevo usuario" : "Login"}</h2>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <div className="password-input-container">
+            <input
+              type={showPassword ? "text" : "password"} // Cambia entre "text" y "password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <span
+              className="password-toggle"
+              onClick={togglePasswordVisibility}
             >
-              <path
-                fill="#4285F4"
-                d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
-              ></path>
-              <path
-                fill="#34A853"
-                d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"
-              ></path>
-              <path
-                fill="#FBBC05"
-                d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782"
-              ></path>
-              <path
-                fill="#EB4335"
-                d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"
-              ></path>
-            </svg>
-            Continue with Google
+              <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+            </span>
+          </div>
+
+          {isRegistering && (
+            <div className="password-input-container">
+              <input
+                type={showConfirmPassword ? "text" : "password"} // Cambia entre "text" y "password"
+                placeholder="Repeat Password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+              <span
+                className="password-toggle"
+                onClick={toggleConfirmPasswordVisibility}
+              >
+                <FontAwesomeIcon
+                  icon={showConfirmPassword ? faEyeSlash : faEye}
+                />
+              </span>
+            </div>
+          )}
+          <button
+            className="button"
+            onClick={isRegistering ? handleRegister : handleLogin}
+          >
+            {isRegistering ? "Add new user" : "Login"}
           </button>
+          <p
+            onClick={() => setIsRegistering(!isRegistering)}
+            className="toggle-link"
+          >
+            {isRegistering
+              ? "¿Ya tienes una cuenta? Inicia sesión"
+              : "¿No tienes una cuenta? Regístrate"}
+          </p>
           {error && <p>{error}</p>}
         </div>
       </div>
